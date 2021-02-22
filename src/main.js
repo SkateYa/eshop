359ad72cfc86c84802c05d922cfad96edecd040b
zhangxinya <1805595661@qq.com>
2021-02-22 09:28:15 +0800

    优化

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import ElementUI from 'element-ui';
 import App from './App.vue'
@@ -9,7 +8,6 @@ import 'element-ui/lib/theme-chalk/index.css';
 
 
 Vue.use(Vuex)
-Vue.use(VueRouter)
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 
